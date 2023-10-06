49792c5db3c0c472607be015f6a5ca1d8606794f
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-10-06 19:58:36 +0200

    added favicons, apple touch icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
+import Head from 'next/head';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="favicon.ico" type="image/x-icon" sizes="any" />
+				<link
+					rel="apple-touch-icon"
+					href="apple-touch-icon.png"
+					type="image/png"
+					sizes="180x180"
+				/>
+				<link rel="shortcut icon" href="favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
