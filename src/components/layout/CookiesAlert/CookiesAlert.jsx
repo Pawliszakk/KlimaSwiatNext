915bbef307bb192170425737b1753f86a4f2fdb3
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-04-09 22:36:34 +0200

    added link to privacy policy

--- src/components/layout/CookiesAlert/CookiesAlert.jsx
+++ src/components/layout/CookiesAlert/CookiesAlert.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import classes from './CookiesAlert.module.css';
-
+import Link from 'next/link';
 const CookiesAlert = () => {
 	const [isClickedOk, setIsClickedOk] = useState(true);
 
@@ -27,6 +27,7 @@ const CookiesAlert = () => {
 					<p>
 						Ta strona korzysta z ciasteczek, dalsze korzystanie ze strony
 						oznacza, że zgadzasz się na ich użycie.
+						<Link href="/polityka-prywatnosci">Polityka prywatności</Link>
 					</p>
 					<button onClick={buttonClickHandler}>OK</button>
 				</div>
