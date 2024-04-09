b4397c2ff0f5e9f22f8f5d06ab86c58727376861
Pawliszakk <oskar.pawliszak1@o2.pl>
2024-04-09 22:23:20 +0200

    added cookie alert

--- src/components/layout/CookiesAlert/CookiesAlert.jsx
+++ src/components/layout/CookiesAlert/CookiesAlert.jsx
@@ -36,29 +36,3 @@ const CookiesAlert = () => {
 };
 
 export default CookiesAlert;
-
-// import { useState } from 'react';
-// import classes from './CookiesAlert.module.css';
-// const CookiesAlert = () => {
-// 	const [isClickedOk, setIsClickedOk] = useState(false);
-
-// 	const buttonClickHandler = () => setIsClickedOk(true);
-
-// 	return (
-// 		<>
-// 			{isClickedOk ? (
-// 				<div></div>
-// 			) : (
-// 				<div className={classes.box}>
-// 					<p>
-// 						Ta strona korzysta z ciasteczek, dalsze korzystanie ze strony
-// 						oznacza, że zgadzasz się na ich użycie.
-// 					</p>
-// 					<button onClick={buttonClickHandler}>OK</button>
-// 				</div>
-// 			)}
-// 		</>
-// 	);
-// };
-
-// export default CookiesAlert;
